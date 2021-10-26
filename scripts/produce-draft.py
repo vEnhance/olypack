11b0f779a3614a39c92a6424eee6ca3aca4771ef
Evan Chen <evan@evanchen.cc>
2021-10-26 16:02:15 -0400

    feat: produce drafts for testsolving

--- scripts/produce-draft.py
+++ scripts/produce-draft.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import tempfile
+
 import yaml
 
 __version__ = "2020-05"
@@ -8,7 +9,7 @@ __version__ = "2020-05"
 tmp_dir_path = tempfile.mkdtemp()
 OUTPUT_PATH = 'output/'
 
-with open('output/user') as f:
+with open('password') as f:
 	PASSWORD = f.readline().strip()
 
 with open('data.yaml') as f:
@@ -17,20 +18,26 @@ with open('data.yaml') as f:
 	DRAFT_PATH = yaml_data['draft']
 	assert os.path.exists(DRAFT_PATH), DRAFT_PATH + " does not exist"
 
+code_to_path = {}
+with open('output/authors.tsv') as f:
+	for line in f:
+		code = line.strip().split('\t')[0]
+		path = line.strip().split('\t')[-1]
+		code_to_path[code] = path
+
 N = 0
 for n, day in chosen_problems.items():
 	# Make problems
-	problems_filename = "problems-day%d.pdf" % n
+	problems_filename = "draft-problems-day%d.pdf" % n
 	problems_tmp_path = os.path.join(tmp_dir_path, problems_filename)
 	problems_enc_path = os.path.join(OUTPUT_PATH, problems_filename)
-	subprocess.call(["qpdf",
-		"--empty", "--pages",
-		DRAFT_PATH, str(n),
-		"--", problems_tmp_path])
-	subprocess.call(["qpdf",
-		"--encrypt", PASSWORD, PASSWORD, '128',
-		'--print=none', '--modify=none',
-		'--', problems_tmp_path, problems_enc_path])
+	subprocess.call(["qpdf", "--empty", "--pages", DRAFT_PATH, str(n), "--", problems_tmp_path])
+	subprocess.call(
+		[
+			"qpdf", "--encrypt", PASSWORD, PASSWORD, '128', '--print=none', '--modify=none', '--',
+			problems_tmp_path, problems_enc_path
+		]
+	)
 
 	# Compile solutions
 	raw_sol_tex_path = os.path.join(tmp_dir_path, "s%d.tex" % n)
@@ -38,13 +45,14 @@ for n, day in chosen_problems.items():
 	with open(raw_sol_tex_path, "w") as f:
 		print(r"\documentclass[11pt]{scrartcl}", file=f)
 		print(r"\usepackage[sexy]{evan}", file=f)
-		print(r"\title{Preliminary solutions to day %d}" %n, file=f)
+		print(r"\title{Preliminary solutions to day %d}" % n, file=f)
 		print(r"\author{Confidential}", file=f)
 		print(r"\begin{document}", file=f)
-		print(r"\setcounter{section}{%d}" %N, file=f)
+		print(r"\setcounter{section}{%d}" % N, file=f)
 		print("\n", file=f)
 
-		for problem_path in day:
+		for code in day:
+			problem_path = code_to_path[code]
 			with open(problem_path) as g:
 				text = ''.join(g.readlines())
 				stuff = text.split('\n---\n')
@@ -52,7 +60,7 @@ for n, day in chosen_problems.items():
 					metadata_raw, prob, sol = stuff[0:3]
 				except ValueError:
 					print(stuff)
-					raise ValueError("Couldn't process " + prob_source)
+					raise ValueError(f"Couldn't process {problem_path}")
 				prob = prob.strip()
 				sol = sol.strip()
 				metadata_dict = yaml.load(metadata_raw, Loader=yaml.FullLoader)
@@ -70,10 +78,12 @@ for n, day in chosen_problems.items():
 	subprocess.call(["latexmk", "-cd", raw_sol_tex_path])
 
 	# Make solutions
-	solutions_filename = "solutions-day%d.pdf" % n
+	solutions_filename = "draft-soln-day%d.pdf" % n
 	solutions_tmp_path = raw_sol_pdf_path
 	solutions_enc_path = os.path.join(OUTPUT_PATH, solutions_filename)
-	subprocess.call(["qpdf",
-		"--encrypt", PASSWORD, PASSWORD, '128',
-		'--print=none', '--modify=none',
-		'--', solutions_tmp_path, solutions_enc_path])
+	subprocess.call(
+		[
+			"qpdf", "--encrypt", PASSWORD, PASSWORD, '128', '--print=none', '--modify=none', '--',
+			solutions_tmp_path, solutions_enc_path
+		]
+	)
