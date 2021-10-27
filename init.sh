34e264768db191f3e095fd00c3f175cf642214b9
Evan Chen <evan@evanchen.cc>
2021-10-26 20:21:33 -0400

    fix(init): missed wildcard

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
-cp -r template/ ..
+cp -r template/* ..
 
 mkdir ../notes
