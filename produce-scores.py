9bd8e08722a53881ac4adcc5dba60e63719c6a60
Andrew Gu <a1267ab@gmail.com>
2024-02-17 15:26:11 -0500

    Move processing steps from produce_scores to utils

--- produce-scores.py
+++ produce-scores.py
@@ -1,115 +1,11 @@
-import collections
-import csv
-from typing import DefaultDict, List
-
-from utils import all_problems, jinja_env
+from utils import all_problems, all_problems_with_ratings, jinja_env
 
 __version__ = "2024-02"
 
-quality_indices: DefaultDict[str, List[float]] = collections.defaultdict(list)
-difficulty_indices: DefaultDict[str, List[float]] = collections.defaultdict(list)
-quality_avgs: DefaultDict[str, float] = collections.defaultdict(float)
-difficulty_avgs: DefaultDict[str, float] = collections.defaultdict(float)
-slugs = {}
-authors = {}
-
-
-def avg(x) -> float:
-    if len(x) == 0:
-        return 0
-    else:
-        return sum(x) / len(x)
-
-
-QUALITY_SCALE = ["UNSUITABLE", "MEDIOCRE", "ACCEPTABLE", "NICE", "EXCELLENT"]
-QUALITY_WEIGHTS = [-0.75, -0.5, 0, 1, 1.5]
-
-DIFFICULTY_SCALE = ["IMO1", "IMO1,IMO2", "IMO2", "IMO2,IMO3", "IMO3"]
-DIFFICULTY_WEIGHTS = [1, 1.5, 2, 2.5, 3]
-
-
-# Read data
-with open("ratings.tsv", "r") as f:
-    reader = csv.DictReader(f, delimiter="\t")
-    for row in reader:
-        for key in row.keys():
-            if key is None or row[key] is None:
-                continue
-            if "quality rating" in key:
-                p = key[key.index("[") + 1 :]
-                p = p[: p.index(" ")]
-                r = row[key]
-                r = r.replace(" ", "").upper()
-                if r in QUALITY_SCALE:
-                    quality_indices[p].append(QUALITY_SCALE.index(r))
-            if "difficulty rating" in key:
-                p = key[key.index("[") + 1 :]
-                p = p[: p.index(" ")]
-                r = row[key]
-                r = r.replace(" ", "").upper()
-                if r in DIFFICULTY_SCALE:
-                    difficulty_indices[p].append(DIFFICULTY_SCALE.index(r))
-
-with open("output/authors.tsv") as f:
-    for line in f:
-        p, author, slug, filename, *_ = line.strip().split("\t")
-        authors[p] = author
-        slugs[p] = slug
-        quality_avgs[p] = avg([QUALITY_WEIGHTS[i] for i in quality_indices[p]])
-        difficulty_avgs[p] = avg([DIFFICULTY_WEIGHTS[i] for i in difficulty_indices[p]])
-
-
-def get_color_string(x, scale_min, scale_max, color_min, color_max):
-    m = (scale_max + scale_min) / 2
-    a = min(int(100 * 2 * abs(x - m) / (scale_max - scale_min)), 100)
-    color = color_min if x < m else color_max
-    return r"\rowcolor{%s!%d}" % (color, a) + "\n"
-
-
-quality_color_strings = {
-    key: get_color_string(
-        quality_avgs[key], QUALITY_WEIGHTS[0], QUALITY_WEIGHTS[-1], "Salmon", "green"
-    ).strip()
-    for key in quality_avgs
-}
-
-difficulty_color_strings = {
-    key: get_color_string(
-        difficulty_avgs[key],
-        DIFFICULTY_WEIGHTS[0],
-        DIFFICULTY_WEIGHTS[-1],
-        "cyan",
-        "orange",
-    ).strip()
-    for key in difficulty_avgs
-}
-
-
-def serialized(key):
-    return {
-        "key": key,
-        "quality": quality_indices[key],
-        "difficulty": difficulty_indices[key],
-        "quality_avg": quality_avgs[key],
-        "difficulty_avg": difficulty_avgs[key],
-        "quality_color": quality_color_strings[key],
-        "difficulty_color": difficulty_color_strings[key],
-        "slug": slugs[key],
-        "overall_popularity_key": (-quality_avgs[key], key),
-        "subject_popularity_key": (key[0], -quality_avgs[key], key),
-        "overall_difficulty_key": (-difficulty_avgs[key], key),
-        "subject_difficulty_key": (key[0], -difficulty_avgs[key], key),
-    }
-
-
-problems = [serialized(key) for key in quality_indices]
-
-filtered_problems = [
-    serialized(key) for key in quality_indices if quality_avgs[key] >= 0
-]
-
 with open("final-report/table.txt", "w") as f:
-    if len(difficulty_indices) > 0 or len(quality_indices) > 0:
+    problems = all_problems_with_ratings()
+    filtered_problems = [p for p in problems if p["quality_avg"] >= 0]
+    if problems:
         env = jinja_env()
         template = env.get_template("table.txt.jinja")
         f.write(
@@ -120,16 +16,6 @@ with open("final-report/table.txt", "w") as f:
         )
     else:
         f.write("No ratings to display here yet\n")
-
-with open("output/summary.csv", "w") as f:
-    for p in sorted(quality_indices.keys()):
-        qs = ",".join(
-            str(quality_indices[p].count(x)) for x in range(len(QUALITY_WEIGHTS))
-        )
-        ds = ",".join(
-            str(difficulty_indices[p].count(x)) for x in range(len(DIFFICULTY_WEIGHTS))
-        )
-        print(f'{p},"{slugs[p]}","{authors[p]}",{qs},{ds}', file=f)
 
 with open("final-report/comments.tex", "w") as f:
     env = jinja_env()
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
+import collections
+import csv
+from typing import Any, DefaultDict
+
 import yaml
 from jinja2 import Environment, FileSystemLoader
+
+QUALITY_SCALE = ["UNSUITABLE", "MEDIOCRE", "ACCEPTABLE", "NICE", "EXCELLENT"]
+QUALITY_WEIGHTS = [-0.75, -0.5, 0, 1, 1.5]
+
+DIFFICULTY_SCALE = ["IMO1", "IMO1,IMO2", "IMO2", "IMO2,IMO3", "IMO3"]
+DIFFICULTY_WEIGHTS = [1, 1.5, 2, 2.5, 3]
 
 
 def jinja_env():
@@ -12,7 +22,7 @@ def get_individual_authors(author_string: str) -> list[str]:
     return author_string.split(", ")
 
 
-def problem_data_from_filename(filename: str) -> dict:
+def problem_data_from_filename(filename: str) -> dict[str, Any]:
     with open(filename) as g:
         text = g.read()
         stuff = text.split("\n---\n")
@@ -42,7 +52,7 @@ def problem_data_from_filename(filename: str) -> dict:
     }
 
 
-def all_problems():
+def all_problems() -> dict[str, list[dict[str, Any]]]:
     with open("data.yaml") as f:
         problem_files = yaml.load(f, Loader=yaml.FullLoader)["packet"]
 
@@ -62,4 +72,97 @@ def all_problems():
                     "pnum_no_dash": pnum_no_dash,
                 }
             )
+    return problems
+
+
+def avg(x) -> float:
+    if len(x) == 0:
+        return 0
+    else:
+        return sum(x) / len(x)
+
+
+def get_color_string(x, scale_min, scale_max, color_min, color_max):
+    m = (scale_max + scale_min) / 2
+    a = min(int(100 * 2 * abs(x - m) / (scale_max - scale_min)), 100)
+    color = color_min if x < m else color_max
+    return r"\rowcolor{%s!%d}" % (color, a) + "\n"
+
+
+def all_problems_with_ratings() -> list[dict[str, Any]]:
+    quality_indices: DefaultDict[str, list[float]] = collections.defaultdict(list)
+    difficulty_indices: DefaultDict[str, list[float]] = collections.defaultdict(list)
+    quality_avgs: DefaultDict[str, float] = collections.defaultdict(float)
+    difficulty_avgs: DefaultDict[str, float] = collections.defaultdict(float)
+
+    problems = [item for subject, items in all_problems().items() for item in items]
+    all_keys = [problem["pnum"] for problem in problems]
+
+    # Read data
+    with open("ratings.tsv", "r") as f:
+        reader = csv.DictReader(f, delimiter="\t")
+        for row in reader:
+            for key in row.keys():
+                if key is None or row[key] is None:
+                    continue
+                if "quality rating" in key:
+                    p = key[key.index("[") + 1 :]
+                    p = p[: p.index(" ")]
+                    r = row[key]
+                    r = r.replace(" ", "").upper()
+                    if r in QUALITY_SCALE:
+                        quality_indices[p].append(QUALITY_SCALE.index(r))
+                if "difficulty rating" in key:
+                    p = key[key.index("[") + 1 :]
+                    p = p[: p.index(" ")]
+                    r = row[key]
+                    r = r.replace(" ", "").upper()
+                    if r in DIFFICULTY_SCALE:
+                        difficulty_indices[p].append(DIFFICULTY_SCALE.index(r))
+
+    for key in all_keys:
+        quality_avgs[key] = avg([QUALITY_WEIGHTS[i] for i in quality_indices[key]])
+        difficulty_avgs[key] = avg(
+            [DIFFICULTY_WEIGHTS[i] for i in difficulty_indices[key]]
+        )
+
+    quality_color_strings = {
+        key: get_color_string(
+            quality_avgs[key],
+            QUALITY_WEIGHTS[0],
+            QUALITY_WEIGHTS[-1],
+            "Salmon",
+            "green",
+        ).strip()
+        for key in all_keys
+    }
+
+    difficulty_color_strings = {
+        key: get_color_string(
+            difficulty_avgs[key],
+            DIFFICULTY_WEIGHTS[0],
+            DIFFICULTY_WEIGHTS[-1],
+            "cyan",
+            "orange",
+        ).strip()
+        for key in all_keys
+    }
+
+    def rating_info(key):
+        return {
+            "quality": quality_indices[key],
+            "difficulty": difficulty_indices[key],
+            "quality_avg": quality_avgs[key],
+            "difficulty_avg": difficulty_avgs[key],
+            "quality_color": quality_color_strings[key],
+            "difficulty_color": difficulty_color_strings[key],
+            "overall_popularity_key": (-quality_avgs[key], key),
+            "subject_popularity_key": (key[0], -quality_avgs[key], key),
+            "overall_difficulty_key": (-difficulty_avgs[key], key),
+            "subject_difficulty_key": (key[0], -difficulty_avgs[key], key),
+        }
+
+    for i in range(len(problems)):
+        problems[i].update(rating_info(problems[i]["pnum"]))
+
     return problems
