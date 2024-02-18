e220f9ae49c18a90b2bd0babebf0fa2f17b7a378
Andrew Gu <a1267ab@gmail.com>
2024-02-17 23:25:23 -0500

    Move warning location

--- produce-test.py
+++ produce-test.py
@@ -1,15 +1,6 @@
-import os
-
 from utils import chosen_problems, jinja_env
 
 env = jinja_env()
-
-if os.path.exists("test/problems-1.tex"):
-    res = input(
-        "Problem files already exist. If you are using those to edit the statements, note that this command will OVERWRITE those files. Continue? (y/N)"
-    )
-    if res.lower() != "y":
-        exit()
 
 for day, problems in chosen_problems().items():
     with open(f"test/problems-{day}.tex", "w") as f:
