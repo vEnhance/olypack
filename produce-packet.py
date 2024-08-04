6cd9755162486603447078d6caaa2f01bc63a3c4
Andrew Gu <a1267ab@gmail.com>
2024-08-03 22:12:01 -0400

    Minor fixes, include authors whose problems weren't included in packet

--- produce-packet.py
+++ produce-packet.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
-__version__ = "2024-02"
+__version__ = "2024-08"
 
+import os
 import yaml
 
-from utils import all_problems, jinja_env
+from utils import all_problems, jinja_env, problem_data_from_filename
 
-with open("data.yaml") as f:
-    problem_files = yaml.load(f, Loader=yaml.FullLoader)["packet"]
-
-total_problems = 0
 
 problems = all_problems()
 unique_authors = set()
-for subject, problem_list in problems.items():
-    total_problems += len(problem_list)
-    for problem in problem_list:
+
+# We look at all the files in source/ so that we find authors who submitted but
+# didn't have any problems included in the packet
+problems_directory = "source/"
+for filename in os.listdir(problems_directory):
+    if filename.endswith(".tex"):
+        filepath = os.path.join(problems_directory, filename)
+        problem = problem_data_from_filename(filepath)
         unique_authors.update(problem["split_authors"])
+
 
 with open("packet/uniqauthor.txt", "w") as f:
     f.write(",\n".join(sorted(unique_authors)))
@@ -34,7 +37,7 @@ with open("packet/data-solns.tex", "w") as f:
 
 with open("packet/data-index.tex", "w") as f:
     template = env.get_template("data-index.tex.jinja")
-    f.write(template.render(problems=problems, total_problems=total_problems))
+    f.write(template.render(problems=problems))
 
 with open("output/form-script.js", "w") as f:
     template = env.get_template("form-script.js.jinja")
