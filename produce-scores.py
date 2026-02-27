001e5fe65bc41c3cb467d9444c2017ca1de4ca25
mil246 <milan.haiman@gmail.com>
2026-02-27 17:18:54 +0100

    refactor: Add QUALITY_CUTOFF parameter (#8)
    * Update utils.py
    
    * Update produce-scores.py

--- produce-scores.py
+++ produce-scores.py
@@ -1,10 +1,10 @@
-from utils import all_problems, all_problems_with_ratings, chosen_problems, jinja_env
+from utils import all_problems, all_problems_with_ratings, chosen_problems, jinja_env, QUALITY_CUTOFF
 
 __version__ = "2024-02"
 
 with open("final-report/table.tex", "w") as f:
     problems = all_problems_with_ratings()
-    filtered_problems = [p for p in problems if p["quality_avg"] >= 0]
+    filtered_problems = [p for p in problems if p["quality_avg"] >= QUALITY_CUTOFF]
     if problems:
         env = jinja_env()
         template = env.get_template("table.tex.jinja")
